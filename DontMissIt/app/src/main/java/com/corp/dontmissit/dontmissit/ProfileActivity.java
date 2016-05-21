861191be05278c539e263d24c718d977099dcce1
Huyzy <huyngo.developer@gmail.com>
2016-05-21 16:15:32 -0700

    Allow SMS sending. You can test via ProfileActivity save button.

--- DontMissIt/app/src/main/java/com/corp/dontmissit/dontmissit/ProfileActivity.java
+++ DontMissIt/app/src/main/java/com/corp/dontmissit/dontmissit/ProfileActivity.java
@@ -1,9 +1,13 @@
 package com.corp.dontmissit.dontmissit;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
@@ -15,12 +19,25 @@ import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    public static boolean HAS_SMS_PERMISSION;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Request permissions for sending SMS
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        123);
+
+        }
 
         // Save button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.saveBtn);
@@ -31,21 +48,25 @@ public class ProfileActivity extends AppCompatActivity {
                 Snackbar.make(view, "Your message has been saved!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
-                SmsManager smsManager = SmsManager.getDefault();
-                smsManager.sendTextMessage("2536322529", null, "Testing from Don'tMiss app", null, null);
+                // Send text message
+                if (HAS_SMS_PERMISSION) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage("2536322529", null, "Testing from DontMiss app", null, null);
+                }
             }
         });
+
 
         int numGC = 3;
         final String[] giftCardNames = {"iTunes", "Victoria Secret", "Walmart"};
         int imageViewId = R.id.giftcard_1;
 
         // Selected Gift Card Holder Variables
-        final LinearLayout linearLayout = (LinearLayout)findViewById(R.id.selected_gift_holder);
-        final TextView textView = (TextView)findViewById(R.id.gift_card_data);
+        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.selected_gift_holder);
+        final TextView textView = (TextView) findViewById(R.id.gift_card_data);
 
-        for (int i = 0; i < numGC; i ++) {
-            ImageButton imageButton = (ImageButton)findViewById(imageViewId + i);
+        for (int i = 0; i < numGC; i++) {
+            ImageButton imageButton = (ImageButton) findViewById(imageViewId + i);
             assert imageButton != null;
             final int position = i;
             imageButton.setOnClickListener(new View.OnClickListener() {
@@ -63,7 +84,7 @@ public class ProfileActivity extends AppCompatActivity {
         }
 
         // Set reset button
-        Button resetBtn = (Button)findViewById(R.id.reset_button);
+        Button resetBtn = (Button) findViewById(R.id.reset_button);
         assert resetBtn != null;
         resetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,6 +93,19 @@ public class ProfileActivity extends AppCompatActivity {
                 linearLayout.setVisibility(View.GONE);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 123: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    HAS_SMS_PERMISSION = true;
+                }
+            }
+        }
     }
 
 }
